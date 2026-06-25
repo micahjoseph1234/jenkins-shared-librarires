c65fe5a96ac380c6192ec86e91466c83a6e63094
Micah Joseph <72503783+micahjoseph1234@users.noreply.github.com>
2026-06-26 03:53:24 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
+    sh "docker build -t ${ProjectName}:${ImageTag} ."
+    sh "docker tag ${ProjectName}:${ImageTag} ${DockerHubUser}/${ProjectName}:${ImageTag}"
 }
