cc42581a0eb678df20acaa4eaa75b0f5c3ed3114
Micah Joseph <72503783+micahjoseph1234@users.noreply.github.com>
2026-06-26 03:27:58 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,17 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+def call(String Project, String ImageTag, String dockerhubuser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerHubCred',
+            passwordVariable: 'dockerHubPass',
+            usernameVariable: 'dockerHubUser'
+        )
+    ]) {
+
+        sh '''
+        echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+        '''
+    }
+
+    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
